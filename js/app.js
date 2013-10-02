345182992106486081896d1dcd8a1cfb129735a0
Pavel Orlov <garffans@outlook.com>
2013-10-02 19:24:04 +0400

    move to workspace

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-function person(name, age, group){
-	this.name = name;
-	this.age = age;
-	this.group = group;
-} 
 
-
-var pers = new person('Pavel', 19, 'ПВ1013');
-
-console.log(pers.name + ', age : ' + pers.age);
